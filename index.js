6946f9de72fd00cad369559c559f0d6eb9e83414
Neal JIn Oh <no4259@gmail.com>
2020-01-26 01:36:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
